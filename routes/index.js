58766afe1530250f9d73aa79d957a9835fe114a9
Alicia Urdiales <aliurdialess@gmail.com>
2023-11-07 14:34:14 +0000

    Settings done

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@ const router = require("express").Router();
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+const celebritiesRouter = require("./celebrities.routes.js")
+router.use("/celebrities", celebritiesRouter)
+
+const moviesRouter = require("./movies.routes.js")
+router.use("/movies", moviesRouter)
 
 module.exports = router;
